68f91606c6743708ef0b037811534c8019c1b92e
EliArtist <elias-kuenstler@web.de>
2017-08-11 22:01:54 +0200

    changed structure

--- www/ContextMenu.js
+++ www/ContextMenu.js
@@ -1,11 +1,11 @@
 console.log(":::::CONTEXTMENUJS:::::");
 
-function ContextMenu() {
-    this._test_ = "Hallo Welt!";
-}
+var ContextMenu = {
+    __test: "Hallo Welt";
 
-ContextMenu.prototype.getTest = function() {
-    return this._test_;
+    getTest: function() {
+        return ContextMenu.__test;
+    }
 }
 
 module.exports = ContextMenu;
