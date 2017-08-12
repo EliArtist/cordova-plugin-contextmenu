d01fcfb47abc96e1994cb8bcbb5b69726b287017
EliArtist <elias-kuenstler@web.de>
2017-08-12 17:01:44 +0200

    extended CordovaWebView

--- src/android/Contextmenu.java
+++ src/android/Contextmenu.java
@@ -9,12 +9,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import MyWebView;
+
 public class Contextmenu extends CordovaPlugin {
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        MyWebView myWebView = (MyWebView) webView;
+
         super.initialize(cordova, webView);
-        //        MyWebView myWebView = (MyWebView) webView;
     }
 
     @Override
@@ -31,54 +34,3 @@ public class Contextmenu extends CordovaPlugin {
         callbackContext.success("This was a great success!");
     }
 }
-
-//protected class MyWebView extends CordovaWebView {
-//
-//    private ActionMode mActionMode = null;
-//
-//    @Override
-//    public void onActionModeStarted(ActionMode mode) {
-//        if (mActionMode == null) {
-//            mActionMode = mode;
-//            Menu menu = mode.getMenu();
-//            // Remove the default menu items (select all, copy, paste, search)
-//            menu.clear();
-//
-//            // If you want to keep any of the defaults,
-//            // remove the items you don't want individually:
-//            // menu.removeItem(android.R.id.[id_of_item_to_remove])
-//
-//            // Inflate your own menu items
-////            mode.getMenuInflater().inflate(R.menu.my_custom_menu, menu);
-//        }
-//
-//        super.onActionModeStarted(mode);
-//    }
-//
-//    // This method is what you should set as your item's onClick
-//    // <item android:onClick="onContextualMenuItemClicked" />
-////    public void onContextualMenuItemClicked(MenuItem item) {
-////        switch (item.getItemId()) {
-////            case R.id.example_item_1:
-////                // do some stuff
-////                break;
-////            case R.id.example_item_2:
-////                // do some different stuff
-////                break;
-////            default:
-////                // ...
-////                break;
-////        }
-////
-////        // This will likely always be true, but check it anyway, just in case
-////        if (mActionMode != null) {
-////            mActionMode.finish();
-////        }
-////    }
-//
-//    @Override
-//    public void onActionModeFinished(ActionMode mode) {
-//        mActionMode = null;
-//        super.onActionModeFinished(mode);
-//    }
-//}
---
+++ src/android/MyWebView.java
@@ -0,0 +1,50 @@
+protected class MyWebView extends CordovaWebView {
+
+    private ActionMode mActionMode = null;
+
+    @Override
+    public void onActionModeStarted(ActionMode mode) {
+        if (mActionMode == null) {
+            mActionMode = mode;
+            Menu menu = mode.getMenu();
+            // Remove the default menu items (select all, copy, paste, search)
+            menu.clear();
+
+            // If you want to keep any of the defaults,
+            // remove the items you don't want individually:
+            // menu.removeItem(android.R.id.[id_of_item_to_remove])
+
+            // Inflate your own menu items
+//            mode.getMenuInflater().inflate(R.menu.my_custom_menu, menu);
+        }
+
+        super.onActionModeStarted(mode);
+    }
+
+    // This method is what you should set as your item's onClick
+    // <item android:onClick="onContextualMenuItemClicked" />
+//    public void onContextualMenuItemClicked(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.example_item_1:
+//                // do some stuff
+//                break;
+//            case R.id.example_item_2:
+//                // do some different stuff
+//                break;
+//            default:
+//                // ...
+//                break;
+//        }
+//
+//        // This will likely always be true, but check it anyway, just in case
+//        if (mActionMode != null) {
+//            mActionMode.finish();
+//        }
+//    }
+
+    @Override
+    public void onActionModeFinished(ActionMode mode) {
+        mActionMode = null;
+        super.onActionModeFinished(mode);
+    }
+}
