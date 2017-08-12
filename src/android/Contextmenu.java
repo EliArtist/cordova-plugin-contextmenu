93fdc2993ee6125243782ff81e3832c907ad709e
EliArtist <elias-kuenstler@web.de>
2017-08-12 16:08:19 +0200

    added logic

--- src/android/Contextmenu.java
+++ src/android/Contextmenu.java
@@ -13,20 +13,73 @@ import org.json.JSONObject;
 public class Contextmenu extends CordovaPlugin {
 
     @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        MyWebView myWebView = (MyWebView) webView;
+        super.initialize(cordova, myWebView);
+    }
+
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("echo")) {
+        if (action.equals("hideContextMenu")) {
             String message = args.getString(0);
-            this.echo(message, callbackContext);
+            this.hideContextMenu(callbackContext);
             return true;
         }
         return false;
     }
 
-    private void echo(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    private void hideContextMenu(CallbackContext callbackContext) {
+        callbackContext.success("This was a great success!");
+    }
+}
+
+protected class MyWebView extends CordovaWebView {
+
+    private ActionMode mActionMode = null;
+
+    @Override
+    public void onActionModeStarted(ActionMode mode) {
+        if (mActionMode == null) {
+            mActionMode = mode;
+            Menu menu = mode.getMenu();
+            // Remove the default menu items (select all, copy, paste, search)
+            menu.clear();
+
+            // If you want to keep any of the defaults,
+            // remove the items you don't want individually:
+            // menu.removeItem(android.R.id.[id_of_item_to_remove])
+
+            // Inflate your own menu items
+//            mode.getMenuInflater().inflate(R.menu.my_custom_menu, menu);
         }
+
+        super.onActionModeStarted(mode);
+    }
+
+    // This method is what you should set as your item's onClick
+    // <item android:onClick="onContextualMenuItemClicked" />
+//    public void onContextualMenuItemClicked(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.example_item_1:
+//                // do some stuff
+//                break;
+//            case R.id.example_item_2:
+//                // do some different stuff
+//                break;
+//            default:
+//                // ...
+//                break;
+//        }
+//
+//        // This will likely always be true, but check it anyway, just in case
+//        if (mActionMode != null) {
+//            mActionMode.finish();
+//        }
+//    }
+
+    @Override
+    public void onActionModeFinished(ActionMode mode) {
+        mActionMode = null;
+        super.onActionModeFinished(mode);
     }
 }
--- www/ContextMenu.js
+++ www/ContextMenu.js
@@ -1,10 +1,21 @@
-console.log(":::::CONTEXTMENUJS:::::");
+var exec = require('cordova/exec');
 
 var ContextMenu = {
     __test: "Hallo Welt",
 
     getTest: function() {
         return ContextMenu.__test;
+    },
+
+    /**
+    * Hide context menu of Android phones
+    *
+    * @param (Function) resultCallback  function to call when context menu is hidden
+    * @param (Function) errorCallback   function to call when there are any errors
+    */
+    hideContextMenu: function(resultCallback, errorCallback) {
+        exec(win, fail, 'ContextMenu', 'hideContextMenu');
+        return "function \"hideContextMenu\" running..";
     }
 }
 
