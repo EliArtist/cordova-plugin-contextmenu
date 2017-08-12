4591a5dfd474a600e31b703563c69e9a3806ff2c
EliArtist <elias-kuenstler@web.de>
2017-08-12 17:07:09 +0200

    dont need to import MyWebView?

--- src/android/Contextmenu.java
+++ src/android/Contextmenu.java
@@ -9,7 +9,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import MyWebView;
+//import MyWebView;
 
 public class Contextmenu extends CordovaPlugin {
 
